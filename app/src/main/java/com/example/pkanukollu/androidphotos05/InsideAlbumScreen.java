23ba171f03a6f07f8d2ed0a66a0b9013055637f2
pkanukollu <pkanukollu>
2017-04-25 10:25:21 -0400

    added picture and useralbum

---
+++ app/src/main/java/com/example/pkanukollu/androidphotos05/InsideAlbumScreen.java
@@ -0,0 +1,13 @@
+package com.example.pkanukollu.androidphotos05;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsideAlbumScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inside_album_screen);
+    }
+}
---
+++ app/src/main/java/com/example/pkanukollu/androidphotos05/UserAlbum.java
@@ -0,0 +1,8 @@
+package com.example.pkanukollu.androidphotos05;
+
+/**
+ * Created by pkanukollu on 4/25/2017.
+ */
+
+public class UserAlbum {
+}
